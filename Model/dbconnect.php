1e17addc5f3a04b2234097e528427053776c9d54
bambambku <bambambku@gmail.com>
2024-11-06 14:53:49 +0000

    bla

--- Model/dbconnect.php
+++ Model/dbconnect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "inventory_management_system.db";
+$dbname = "inventory_management_system_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
