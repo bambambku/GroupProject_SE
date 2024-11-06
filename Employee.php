992cefcb258277b3d6b06d5a2bcda877ed0348b0
Snowy05 <89207349+Snowy05@users.noreply.github.com>
2024-11-06 20:32:15 +0000

    Employee page+CSS

---
+++ Employee.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style-desktop.css" media="screen and (min-width: 1025px)">
+    <title>Employee</title>
+</head>
+<body id="background2">
+    <div class="decoration-bar"></div>
+    <div class="sidebar">
+        <div class="menu-picture-container">
+            <img src="Pictures/logo.png" alt="logo" class="menu-picture">
+            <h2 class="name-txt">John Doe</h2>
+            <p class="job-title-txt">Employee</p>
+        </div>
+        <ul class="menu">
+          <li><a href="">New Sale</a></li>
+          <li><a href="">Stock</a></li>
+          <li class="bttn-logout"><a href="">Logout</a></li>
+        </ul>
+    </div>
+    
+</body>
+</html>
