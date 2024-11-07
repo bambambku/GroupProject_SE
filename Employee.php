4530f09391ca831ba70564720569527c30dcd2e3
Snowy05 <89207349+Snowy05@users.noreply.github.com>
2024-11-07 12:23:49 +0000

    Footer added

--- Employee.php
+++ Employee.php
@@ -7,7 +7,7 @@
     <title>Employee</title>
 </head>
 <body id="background2">
-    <div class="decoration-bar"></div>
+        <div class="decoration-bar"></div>
         <div class="sidebar">
             <div class="menu-picture-container">
                 <img src="Pictures/logo.png" alt="logo" class="menu-picture">
@@ -15,14 +15,24 @@
                     <h2 class="name-txt">John Doe</h2>
                     <p class="job-title-txt">Employee</p>
                 </div>
-            <ul class="menu">
-                <li><a href="">New Sale</a></li>
-                <li><a href="">Stock</a></li>
-                <li class="bttn-logout"><a href="">Logout</a></li>
+                <ul class="menu">
+                    <li><a href="">New Sale</a></li>
+                    <li><a href="">Stock</a></li>
+                    <li class="bttn-logout"><a href="">Logout</a></li>
+                </ul>
+             </div>
+        </div>   
+    <footer>
+    <div class="footer-content">
+        <p>&copy; 2024 Terra Core. All rights reserved.</p>
+        <nav>
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                <li><a href="">Privacy Policy</a></li>
             </ul>
-        </div>
+        </nav>
     </div>
-
-    
+</footer>
 </body>
 </html>
