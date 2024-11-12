63721bbe3d61a84c41b6a026d321c0a4d8e2121f
c2030860 <c2030860@hallam.shu.ac.uk>
2024-11-12 12:38:07 +0000

    Added Folder Structure

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <div class="decoration-bar"></div>
 <div class="sidebar">
     <div class="menu-picture-container">
-        <img src="../Pictures/logo.png" alt="logo" class="menu-picture">
+        <img src="../../Pictures/logo.png" alt="logo" class="menu-picture">
         <div class="underlogo">
             <h2 class="name-txt">John Doe</h2>
             <p class="job-title-txt">Employee</p>
@@ -9,7 +9,7 @@
         <ul class="menu">
             <li><a href="">New Sale</a></li>
             <li><a href="">Stock</a></li>
-            <li class="bttn-logout"><a href="">Logout</a></li>
+            <li class="bttn-logout"><a href="../login/logout.php">Logout</a></li>
         </ul>
     </div>
 </div>   
