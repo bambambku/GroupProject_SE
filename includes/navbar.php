bc8551ff5d195f212a8fec6b2257aa45c87f8c16
Charlie Starling <c2013127@hallam.shu.ac.uk>
2024-11-22 01:10:43 +0000

    Navbar for different links

--- includes/navbar.php
+++ includes/navbar.php
@@ -16,9 +16,85 @@
             <p class="job-title-txt"><?php echo  $_SESSION['user_role']['name']?></p>
         </div>
         <ul class="menu">
-            <li><a href="">New Sale</a></li>
-            <li><a href="">Stock</a></li>
+            <?php 
+            switch($_SESSION['user_role']['name']){
+                case("Employee"):
+                    echo "<li><a href='../employee/sales_index.php'>New Sale</a></li>";
+                    echo "<li><a href=''>Stock</a></li>";
+                    break;
+                case("Admin"):
+                    echo "<li><a href=''>Access Users</a></li>";
+                    echo "<li><a href=''>Create New User</a></li>";
+                    echo "<li><a href=''>Branches</a></li>";
+                    break;
+                case("Stock Manager"):
+                    echo "<li><a href=''>Stock View</a></li>";
+                    echo "<li><a href=''>Add New Product</a></li>";
+                    echo "<li><a href=''>Orders</a></li>";
+                    break;
+                case("Manager"):
+                    echo "<li><a href=''>Employee Details</a></li>";
+                    echo "<li><a href=''>Branch Report</a></li>";
+                    echo "<li><a href=''>Previous Reports</a></li>";
+                    break;
+                case("Director"):
+                    echo "<li><a href=''>Dashboard</a></li>";
+                    echo "<li><a href=''>Reports</a></li>";
+                    break;
+                default:
+                    break;
+            }
+            ?>
             <li class="bttn-logout"><a href="../login/logout.php">Logout</a></li>
         </ul>
     </div>
 </div>   
+
+<!-- 
+
+CREATE DYNAMIC NAVBAR BASED ON USER ROLE
+===========================================
+If the user is Employee {
+    Set the '<ul class="menu"> ... </ul> to have only the employee benchmark pages / actions
+} else if {
+    ... and so on...
+}
+
+
+Manager:
+===========
+[|] Employee Details
+[|] Branch Report
+[|] Previous Reports
+[x] Sign Out
+
+Director: 
+===========
+[] Dashboard
+[] Reports
+[x] Sign Out
+
+
+EMPLOYEE:
+==========
+[|] New Sale
+[|] Stock
+[x] Sign Out
+
+ADMIN:
+==========
+[|] Access Users
+[|] Create New User
+[|] Branches
+[x] Sign Out
+
+Stock Manager:
+==============
+[|] Stock View
+[|] Add New Product
+[|] Orders
+[x] Sign Out
+
+* ALL BASED ON MOCKPLUS WIREFRAME { https://rp.mockplus.com/editor/4p5Fn3_WL/kWbvBYf71 } AS OF { 21/11/2024 } *
+
+-->
